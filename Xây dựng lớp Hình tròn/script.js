fb2bcc165c1e6513bf1f83c08df63a71af2106fd
ManhNang <manhnang12d@gmail.com>
2026-05-26 13:56:32 +0700

    add all file

---
+++ Xây dựng lớp Hình tròn/script.js
@@ -0,0 +1,20 @@
+class Circle {
+    constructor(radius, color){
+        this.radius = radius;
+        this.color = color;
+    }
+
+    getRadius(){
+        return this.radius;
+    }
+
+    getArea(){
+        return this.radius * this.radius * Math.PI;
+    }
+}
+
+let circle = new Circle(2, "red");
+console.log("Radius: " + circle.getRadius() + " |Area: " + circle.getArea());
+
+let circle1 = new Circle(2.582, "blue");
+console.log("Radius: " + circle1.getRadius() + " |Area: " + circle1.getArea());
